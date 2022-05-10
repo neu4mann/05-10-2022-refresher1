c137bfec265e27abf858928f2573c50068d7233d
neu4mann <neumann4@gmail.com>
2022-05-10 10:57:31 -0600

    remove and add .git

---
+++ retrospective/playing.js
@@ -0,0 +1,12 @@
+function countUPTo10() {
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+}
+
+//MAIN
+console.log("Hello World");
+countUPTo10();
+console.log("Bye!");
+console.log("Hello World2");
+
